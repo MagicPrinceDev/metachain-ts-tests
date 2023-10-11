4fda8069755ce5e8d3a9a0bcc55b5a44585587d2
GoldenStateFence <ferditutkun00@gmail.com>
2023-10-11 13:14:12 +0800

    fix estimate

--- tests/test-gas.ts
+++ tests/test-gas.ts
@@ -48,7 +48,7 @@ describeWithMetachain('Metachain RPC (Gas)', (context) => {
 
 	it('eth_estimateGas for contract creation', async function () {
 		// The value returned as an estimation by the evm with estimate mode ON.
-		let oneOffEstimation = 196701;
+		let oneOffEstimation = 227138;
 		// let binarySearchEstimation = binarySearch(oneOffEstimation);
 		// // Sanity check expect a variance of 10%.
 		// expect(estimationVariance(binarySearchEstimation, oneOffEstimation)).to.be.lessThan(1);
@@ -111,7 +111,7 @@ describeWithMetachain('Metachain RPC (Gas)', (context) => {
 	it('eth_estimateGas should handle AccessList alias', async function () {
 		// The value returned as an estimation by the evm with estimate mode ON.
 		// 4300 == 1900 for one key and 2400 for one storage.
-		let oneOffEstimation = 196701 + 4300;
+		let oneOffEstimation = 227138 + 4300;
 		// let binarySearchEstimation = binarySearch(oneOffEstimation);
 		// Sanity check expect a variance of 10%.
 		// expect(estimationVariance(binarySearchEstimation, oneOffEstimation)).to.be.lessThan(1);
@@ -138,12 +138,12 @@ describeWithMetachain('Metachain RPC (Gas)', (context) => {
 			data: Test.bytecode,
 			gasPrice: '0x0',
 		});
-		expect(result).to.equal(196701);
+		expect(result).to.equal(227138);
 		result = await context.web3.eth.estimateGas({
 			from: GENESIS_ACCOUNT,
 			data: Test.bytecode,
 		});
-		expect(result).to.equal(196701);
+		expect(result).to.equal(227138);
 	});
 
 	it('tx gas limit below ETH_BLOCK_GAS_LIMIT', async function () {
