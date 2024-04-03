94e4e83d7558ebe2a10eb440514cc9f557e4ee04
GoldenStateFence <ferditutkun00@gmail.com>
2024-04-03 17:49:12 +0800

    Add more tracer tests

--- tests/test-trace.ts
+++ tests/test-trace.ts
@@ -9,8 +9,15 @@ import {
     GENESIS_ALICE_PRIVATE_KEY,
     INITIAL_BASE_FEE,
 } from './config';
-import { generate, describeWithMetachain, customRequest } from './util';
+import { generate, describeWithMetachain, customRequest, nestedSingle } from './util';
+
+// Import custom tracers
+import BS_TRACER from '../tracer/blockscout_tracer.min.json'
+import BS_TRACER_V2 from '../tracer/blockscout_tracer_v2.min.json'
+
+// Import contracts
 import Incrementor from '../build/contracts/Incrementor.json';
+import Looper from '../build/contracts/Looper.json';
 
 describeWithMetachain('Metachain RPC (Trace)', (context) => {
     // gen(1) is required to enable evm feature in defichain ecosys
@@ -18,6 +25,7 @@ describeWithMetachain('Metachain RPC (Trace)', (context) => {
         expect(await context.web3.eth.getBlockNumber()).to.equal(1);
     });
 
+    // Test 1
     it("should replay over an intermediate state", async function () {
         const TEST_INCREMENTOR_BYTECODE = Incrementor.bytecode;
         const TEST_INCREMENTOR_ABI = Incrementor.abi as AbiItem[];
@@ -65,7 +73,11 @@ describeWithMetachain('Metachain RPC (Trace)', (context) => {
                 },
                 GENESIS_ALICE_PRIVATE_KEY,
             );
-            const { result: data } = await customRequest(context.web3, 'eth_sendRawTransaction', [callTx.rawTransaction]);
+            const { result: data } = await customRequest(
+                context.web3,
+                'eth_sendRawTransaction',
+                [callTx.rawTransaction],
+            );
             txs.push(data);
         }
         await generate(context.client, 1);
@@ -76,11 +88,141 @@ describeWithMetachain('Metachain RPC (Trace)', (context) => {
 
             await context.web3.eth.getTransactionReceipt(txs[index]);
 
-            const { result: traceTx } = await customRequest(context.web3, 'debug_traceTransaction', [txs[index]]);
-            console.log(traceTx);
+            const { result: traceTx } = await customRequest(
+                context.web3,
+                'debug_traceTransaction',
+                [txs[index]],
+            );
 
             const evmResult = context.web3.utils.hexToNumber("0x" + traceTx.returnValue);
             expect(evmResult).to.equal(target);
         }
+    });
+
+    // Test 2
+    it("should trace nested contract calls", async function () {
+        const { result: send } = await nestedSingle(context);
+        await generate(context.client, 1);
+        const { result: traceTx } = await customRequest(
+            context.web3,
+            'debug_traceTransaction',
+            [send],
+        );
+        const logs: any[] = [];
+        for (const log of traceTx.structLogs) {
+            if (logs.length == 1) {
+                logs.push(log);
+            }
+            if (log.op == "RETURN") {
+                logs.push(log);
+            }
+        }
+        expect(logs).to.be.lengthOf(2);
+        expect(logs[0].depth).to.be.equal(2);
+        expect(logs[1].depth).to.be.equal(1);
+    });
+
+    // Test 3
+    it("should use optional disable parameters", async function () {
+        const { result: send } = await nestedSingle(context);
+        await generate(context.client, 1);
+        const { result: traceTx } = await customRequest(
+            context.web3, 'debug_traceTransaction',
+            [send, { disableMemory: true, disableStack: true, disableStorage: true }],
+        );
+        const logs: any[] = [];
+        for (const log of traceTx.structLogs) {
+            const hasStorage = Object.prototype.hasOwnProperty.call(log, "storage");
+            const hasMemory = Object.prototype.hasOwnProperty.call(log, "memory");
+            const hasStack = Object.prototype.hasOwnProperty.call(log, "stack");
+            if (hasStorage || hasMemory || hasStack) {
+                logs.push(log);
+            }
+        }
+        expect(logs.length).to.be.equal(0);
+    });
+
+    // Test 4
+    it("should format as request (Blockscout)", async function () {
+        const { result: send } = await nestedSingle(context);
+        await generate(context.client, 1);
+        const { result: traceTx } = await customRequest(
+            context.web3, 'debug_traceTransaction',
+            [send, { tracer: BS_TRACER.body }],
+        );
+        const entries = traceTx;
+        expect(entries).to.be.lengthOf(2);
+        const resCaller = entries[0];
+        const resCallee = entries[1];
+        expect(resCaller.callType).to.be.equal("call");
+        expect(resCallee.type).to.be.equal("call");
+        expect(resCallee.from).to.be.equal(resCaller.to);
+        expect(resCaller.traceAddress).to.be.empty;
+        expect(resCallee.traceAddress.length).to.be.eq(1);
+        expect(resCallee.traceAddress[0]).to.be.eq(0);
+    });
+
+    // Test 5
+    it("Trace blockscout v2 - AllEthTxTypes", async function () {
+        const { result: send } = await nestedSingle(context);
+        await generate(context.client, 1);
+        const { result: traceTx } = await customRequest(
+            context.web3, 'debug_traceTransaction',
+            [send, { tracer: BS_TRACER_V2.body }],
+        );
+        const entries = traceTx;
+        expect(entries).to.be.lengthOf(2);
+        const resCaller = entries[0];
+        const resCallee = entries[1];
+        expect(resCaller.callType).to.be.equal("call");
+        expect(resCallee.type).to.be.equal("call");
+        expect(resCallee.from).to.be.equal(resCaller.to);
+        expect(resCaller.traceAddress).to.be.empty;
+        expect(resCallee.traceAddress.length).to.be.eq(1);
+        expect(resCallee.traceAddress[0]).to.be.eq(0);
+    });
+
+    // Test 6
+    it("Trace blockscout v2 should trace correctly out of gas tx execution", async function () {
+        const TEST_LOOPER_BYTECODE = Looper.bytecode;
+        const TEST_LOOPER_ABI = Looper.abi as AbiItem[];
+
+        const contract = new context.web3.eth.Contract(TEST_LOOPER_ABI);
+        const tx = await context.web3.eth.accounts.signTransaction(
+            {
+                from: GENESIS_ACCOUNT,
+                data: TEST_LOOPER_BYTECODE,
+                value: '0x00',
+                gasPrice: context.web3.utils.numberToHex(INITIAL_BASE_FEE),
+                gas: '0x100000',
+            },
+            GENESIS_ACCOUNT_PRIVATE_KEY
+        );
+        await customRequest(context.web3, 'eth_sendRawTransaction', [tx.rawTransaction]);
+        await generate(context.client, 1);
+        let receipt0 = await context.web3.eth.getTransactionReceipt(tx.transactionHash);
+        let contractAddress = receipt0.contractAddress;
+        const callTx = await context.web3.eth.accounts.signTransaction(
+            {
+                from: GENESIS_ALICE,
+                to: contractAddress,
+                data: '0x5bec9e67',
+                value: '0x00',
+                gas: '0x100000',
+            },
+            GENESIS_ALICE_PRIVATE_KEY,
+        );
+        const { result: data } = await customRequest(
+            context.web3,
+            'eth_sendRawTransaction',
+            [callTx.rawTransaction],
+        );
+        await generate(context.client, 1);
+        const { result: traceTx } = await customRequest(
+            context.web3, 'debug_traceTransaction',
+            [data, { tracer: BS_TRACER_V2.body }],
+        );
+        expect(traceTx.length).to.be.eq(1);
+        expect(traceTx[0].error).to.be.equal("out of gas");
     });
 });
