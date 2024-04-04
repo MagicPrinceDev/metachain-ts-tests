b626587980999c925662f3a4b8eefe01bfb1addc
GoldenStateFence <ferditutkun00@gmail.com>
2024-04-04 16:40:04 +0800

    Add more tests

--- tests/test-trace-tx-calltracer.ts
+++ tests/test-trace-tx-calltracer.ts
@@ -144,7 +144,10 @@ describeWithMetachain('Metachain RPC (TraceTransaction - CallTracer)', (context)
                 to: contractProxy,
                 gas: '0x100000',
                 value: '0x00',
-                data: abiProxy.methods.call(contractDummy, abiDummy.methods.multiply(42).encodeABI()).encodeABI(),
+                data: abiProxy.methods.call(
+                    contractDummy,
+                    abiDummy.methods.multiply(42).encodeABI(),
+                ).encodeABI(),
             },
             GENESIS_ALICE_PRIVATE_KEY,
         );
@@ -165,5 +168,79 @@ describeWithMetachain('Metachain RPC (TraceTransaction - CallTracer)', (context)
         expect(traceTx.calls[0].from).to.be.eq(contractProxy.toLowerCase());
         expect(traceTx.calls[0].to).to.be.eq(contractDummy.toLowerCase());
         expect(traceTx.calls[0].type).to.be.eq("CALL");
+    });
+
+    // Test 4
+    it("should correctly trace delegatecall subcall", async function () {
+        const TEST_CALL_FORWARDER_BYTECODE = CallForwarder.bytecode;
+        const TEST_CALL_FORWARDER_ABI = CallForwarder.abi as AbiItem[];
+        const nonce = await context.web3.eth.getTransactionCount(GENESIS_ACCOUNT);
+        const abiProxy = new context.web3.eth.Contract(TEST_CALL_FORWARDER_ABI);
+        const tx1 = await context.web3.eth.accounts.signTransaction(
+            {
+                from: GENESIS_ACCOUNT,
+                data: TEST_CALL_FORWARDER_BYTECODE,
+                value: '0x00',
+                gasPrice: context.web3.utils.numberToHex(INITIAL_BASE_FEE),
+                gas: '0x100000',
+                nonce: nonce,
+            },
+            GENESIS_ACCOUNT_PRIVATE_KEY
+        );
+        await customRequest(context.web3, 'eth_sendRawTransaction', [tx1.rawTransaction]);
+
+        const TEST_MULTIPLY_BY_7_BYTECODE = MultiplyBy7.bytecode;
+        const TEST_MULTIPLY_BY_7_ABI = MultiplyBy7.abi as AbiItem[];
+        const abiDummy = new context.web3.eth.Contract(TEST_MULTIPLY_BY_7_ABI);
+        const tx2 = await context.web3.eth.accounts.signTransaction(
+            {
+                from: GENESIS_ACCOUNT,
+                data: TEST_MULTIPLY_BY_7_BYTECODE,
+                value: '0x00',
+                gasPrice: context.web3.utils.numberToHex(INITIAL_BASE_FEE),
+                gas: '0x100000',
+                nonce: nonce + 1,
+            },
+            GENESIS_ACCOUNT_PRIVATE_KEY
+        )
+        await customRequest(context.web3, 'eth_sendRawTransaction', [tx2.rawTransaction]);
+
+        // Deploy contracts
+        await generate(context.client, 1);
+        let receipt1 = await context.web3.eth.getTransactionReceipt(tx1.transactionHash);
+        let receipt2 = await context.web3.eth.getTransactionReceipt(tx2.transactionHash);
+        let contractProxy = receipt1.contractAddress;
+        let contractDummy = receipt2.contractAddress;
+
+        const callTx = await context.web3.eth.accounts.signTransaction(
+            {
+                from: GENESIS_ALICE,
+                to: contractProxy,
+                gas: '0x100000',
+                value: '0x00',
+                data: abiProxy.methods.delegateCall(
+                    contractDummy,
+                    abiDummy.methods.multiply(42).encodeABI(),
+                ).encodeABI(),
+            },
+            GENESIS_ALICE_PRIVATE_KEY,
+        );
+        const { result: data } = await customRequest(
+            context.web3,
+            'eth_sendRawTransaction',
+            [callTx.rawTransaction],
+        );
+        await generate(context.client, 1);
+
+        const { result: traceTx } = await customRequest(
+            context.web3, 'debug_traceTransaction',
+            [data, { tracer: "callTracer" }],
+        );
+        expect(traceTx.from).to.be.eq(GENESIS_ALICE.toLowerCase());
+        expect(traceTx.to).to.be.eq(contractProxy.toLowerCase());
+        expect(traceTx.calls.length).to.be.eq(1);
+        expect(traceTx.calls[0].from).to.be.eq(contractProxy.toLowerCase());
+        expect(traceTx.calls[0].to).to.be.eq(contractDummy.toLowerCase());
+        expect(traceTx.calls[0].type).to.be.eq("DELEGATECALL");
     });
 });
--- tests/test-trace-tx-raw.ts
+++ tests/test-trace-tx-raw.ts
@@ -19,6 +19,7 @@ import {
 
 // Import contracts
 import Incrementor from '../build/contracts/Incrementor.json';
+import TraceFilter from '../build/contracts/TraceFilter.json';
 
 describeWithMetachain('Metachain RPC (TraceTransaction - Raw)', (context) => {
     // gen(1) is required to enable evm feature in defichain ecosys
@@ -54,7 +55,6 @@ describeWithMetachain('Metachain RPC (TraceTransaction - Raw)', (context) => {
         //
         // So we set 5 different target txs for a single block: the 1st, 3 intermediate, and
         // the last.
-        await generate(context.client, 1);
         const totalTxs = 10;
         const targets = [1, 2, 5, 8, 10];
         const txs: any[] = [];
@@ -69,7 +69,6 @@ describeWithMetachain('Metachain RPC (TraceTransaction - Raw)', (context) => {
                     data: contract.methods.incr(1).encodeABI(),
                     nonce: numTxs,
                     value: '0x00',
-                    gasPrice: context.web3.utils.numberToHex(INITIAL_BASE_FEE),
                     gas: '0x100000',
                 },
                 GENESIS_ALICE_PRIVATE_KEY,
@@ -161,8 +160,67 @@ describeWithMetachain('Metachain RPC (TraceTransaction - Raw)', (context) => {
             [callTx.rawTransaction],
         );
         await generate(context.client, 1);
-        const { result: traceTx } = await customRequest(context.web3, 'debug_traceTransaction', [data]);
+        const { result: traceTx } = await customRequest(
+            context.web3,
+            'debug_traceTransaction',
+            [data],
+        );
         expect(traceTx.gas).to.be.eq("0x5208"); // 21_000 gas for a transfer.
     });
 
+    // Test 5
+    it("should not trace call that would produce too big responses", async function () {
+        const TEST_TRACE_FILTER_BYTECODE = TraceFilter.bytecode;
+        const TEST_TRACE_FILTER_ABI = TraceFilter.abi as AbiItem[];
+
+        const contract = new context.web3.eth.Contract(TEST_TRACE_FILTER_ABI);
+        const bytecode = contract
+            .deploy({
+                data: TEST_TRACE_FILTER_BYTECODE,
+                arguments: [false],
+            })
+            .encodeABI();
+        const tx = await context.web3.eth.accounts.signTransaction(
+            {
+                from: GENESIS_ACCOUNT,
+                data: bytecode,
+                value: '0x00',
+                gasPrice: context.web3.utils.numberToHex(INITIAL_BASE_FEE),
+                gas: '0x100000',
+            },
+            GENESIS_ACCOUNT_PRIVATE_KEY
+        );
+        await customRequest(context.web3, 'eth_sendRawTransaction', [tx.rawTransaction]);
+        await generate(context.client, 1);
+        let receipt0 = await context.web3.eth.getTransactionReceipt(tx.transactionHash);
+        let contractAddress = receipt0.contractAddress;
+
+        const callTx = await context.web3.eth.accounts.signTransaction(
+            {
+                from: GENESIS_ALICE,
+                to: contractAddress,
+                gas: '0x800000',
+                value: "0x00",
+                // 100 represents the number of storage modified
+                // 1000 represents the number of simple steps (that will have 100 storage items in trace)
+                data: contract.methods.heavy_steps(100, 1000).encodeABI(),
+            },
+            GENESIS_ALICE_PRIVATE_KEY,
+        );
+        const { result: data } = await customRequest(
+            context.web3,
+            'eth_sendRawTransaction',
+            [callTx.rawTransaction],
+        );
+        await generate(context.client, 1);
+        expect(await customRequest(context.web3, 'debug_traceTransaction', [data])).to.deep.equal({
+            id: 1,
+            jsonrpc: '2.0',
+            error: {
+                message: `Custom error: error calling EVM : \"replayed transaction generated too ` +
+                    `much data. try disabling memory or storage?\"`,
+                code: -32001,
+            },
+        });
+    });
 });
